5cf4a43ffab893f68fb6d8d26b2378335b8c2377
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-26 15:57:09 +0300

    index.html and bootstrap

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ const express = require('express');
 const morgan = require('morgan');
 const stormpath = require('express-stormpath');
 const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const path = require('path');
+
 const config = require('./webpack.config');
 
 const app = express();
 
 const compiler = webpack(config);
-
-app.use(require('webpack-dev-middleware')(compiler), {
+const middleware = webpackMiddleware(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath,
 });
 
+app.use(middleware);
 app.use(morgan('dev'));
 app.use(stormpath.init(app, {
   web: {
     produces: ['application/json'],
   },
 }));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 app.on('stormpath.ready', () => {
   app.listen(3000, 'localhost', (err) => {
